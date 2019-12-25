1a0b5b6d8947f253d1d7598f28ae6e53c621d025
chengkangjian <chengkangjian@megvii.com>
2019-12-25 18:56:37 +0800

    sync code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:english_words/english_words.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import './src/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,81 +15,5 @@ class MyApp extends StatelessWidget {
       ),
       home: Home(),
     );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  HomeState createState() => HomeState();
-}
-
-class HomeState extends State<Home> {
-  final features = {
-    'inbound': 'INBOUND',
-    'inventory': 'INVENTORY',
-    'stockOut': 'STOCK_OUT',
-    'changeWarehouse': 'STOCK_MOVE',
-    'tizong': 'GATHER_PICK',
-    'tally': 'TALLY',
-    'palletInbound': 'WHOLE_STOCK_IN',
-    'palletOutbound': 'WHOLE_STOCK_OUT',
-    'boxOutbound': 'WHOLE_BOX_OUT',
-  };
-
-  final featureImgMap = {
-    'inbound': 'ruku',
-    'inventory': 'pandian',
-    'stockOut': 'chuku',
-    'changeWarehouse': 'yiku',
-    'tizong': 'tizong',
-    'tally': 'lihuo',
-    'palletInbound': 'zhengtuoruku',
-    'palletOutbound': 'zhengtuochuku',
-    'boxOutbound': 'zhengxiangchuku',
-  };
-  final featureNameMap = {
-    'inbound': '入库',
-    'inventory': '盘点',
-    'stockOut': '出库',
-    'changeWarehouse': '移库',
-    'tizong': '提总',
-    'tally': '理货',
-    'palletInbound': '整托入库',
-    'palletOutbound': '整托出库',
-    'boxOutbound': '整箱出库',
-  };
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('HETU河图')),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: _buildFeatures(),
-        ));
-  }
-
-  List<Widget> _buildFeatures() {
-    return features.keys.map((k) {
-      return _buildSingleFeature(k);
-    }).toList();
-  }
-
-  Widget _buildSingleFeature(String k) {
-    return Card(
-        child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        new SvgPicture.asset(
-          'assets/img/' + featureImgMap[k] + '.svg',
-          width: 60,
-          fit: BoxFit.fitWidth,
-          alignment: Alignment.center,
-        ),
-        Text(featureNameMap[k]),
-      ],
-    )));
   }
 }
